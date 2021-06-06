44957e7a4bb22c77de8f2a295508e1c5ad43581c
Markus Dillmann <markus.dillmann@gmx.net>
2021-06-06 16:16:53 +0200

    fixes from Novanic to be able to build

--- bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/api/PostProcessingEnabler.java
+++ bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/api/PostProcessingEnabler.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.netatmo.internal.api;
 
 import java.io.IOException;
@@ -9,6 +21,9 @@ import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
+/**
+ * @author Gaël L'hopital - Initial contribution
+ */
 public class PostProcessingEnabler implements TypeAdapterFactory {
     public interface PostProcessable {
         void postProcess();
